ac17cc6b27bedebdef90c6083bc4f3ca67166e69
Tyler Waters <tyler.waters@gmail.com>
2014-04-17 23:49:02 -0700

    Removing comments monkey patch - using compression option instead

--- assets/css/sass/config.rb
+++ assets/css/sass/config.rb
@@ -7,9 +7,6 @@ sass_dir = "stylesheets"
 images_dir = "../../img"
 javascripts_dir = "../../js"
 
-# Add in our monkey patch to remove all comments
-require "./remove-comments-monkey-patch"
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :compressed
